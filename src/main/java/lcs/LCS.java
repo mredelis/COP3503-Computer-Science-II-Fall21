f9ec59041b6f639b9c372e0f6959f68260da8910
mredelis <edelitamr@gmail.com>
2021-11-02 12:30:07 -0400

    Added assignment 3

---
+++ src/main/java/lcs/LCS.java
@@ -0,0 +1,26 @@
+/* Name: Edelis Molina
+ * Dr. Andrew Steinberg
+ * COP3503 Fall 2021
+ * Programming Assignment 3
+ */
+
+package lcs;
+
+public class LCS {
+    private String str1;
+    private String str2;
+
+    public LCS(String str1, String str2) {
+        this.str1 = str1;
+        this.str2 = str2;
+    }
+
+    //Method to invoke the dynamic programming solution
+    public void lcsDynamicSol() {
+
+    }
+
+    public String getLCS() {
+        return "Edelis";
+    }
+}
