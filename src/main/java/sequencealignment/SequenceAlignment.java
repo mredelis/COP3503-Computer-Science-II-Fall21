8406dc8102e16f16be721078ebf2392ff9e30a8c
mredelis <edelitamr@gmail.com>
2021-11-25 11:39:20 -0500

    in progress

--- src/main/java/sequencealignment/SequenceAlignment.java
+++ src/main/java/sequencealignment/SequenceAlignment.java
@@ -9,13 +9,31 @@ package sequencealignment;
 public class SequenceAlignment {
     private String str1;
     private String str2;
+    private int n;
+    private int m;
+    private int[][] A;
+    private int[][] B;
 
     public SequenceAlignment(String str1, String str2) {
         this.str1 = str1;
         this.str2 = str2;
+
+        this.m = str1.length(); //rows
+        this.n = str2.length(); //columns
+
+        A = new int[m+1][n+1];
+        B = new int[m+1][n+1];
     }
 
-    public void computeAlignment(int findThis){
+    public void computeAlignment(int delta){
+        int i, j;
+
+        //Initialize the table
+        for (i = 1; i <= m; i++)
+            A[i][0] = i*delta; //first column
+        for (j = 0; j <= n; j++)
+            A[0][j] = j*delta; //first row
+
 
     }
 
